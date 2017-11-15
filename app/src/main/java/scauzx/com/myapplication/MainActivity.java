7ef61516c09f20dfc337ce668356c160344e8f47
zhangzexian <zhangzexian@bigo.sg>
2017-11-15 11:37:43 +0800

    commit all

---
+++ app/src/main/java/scauzx/com/myapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package scauzx.com.myapplication;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import static android.os.Build.*;
+
+/**
+ * @author scauzx
+ */
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupView();
+    }
+
+    private void setupView() {
+        initToolBar();
+
+    }
+
+    private void initToolBar() {
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolBar);
+        mToolBar.setPadding(0, OsUtil.getStatusBarHeight(this), 0, 0);
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+
+}
