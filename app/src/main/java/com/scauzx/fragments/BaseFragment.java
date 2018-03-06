4e453e4751b525b295a75f4f449b1962b9dc1dfa
zhangzexian <zhangzexian@bigo.sg>
2018-03-06 16:40:50 +0800

    fix bug

--- app/src/main/java/com/scauzx/fragments/BaseFragment.java
+++ app/src/main/java/com/scauzx/fragments/BaseFragment.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.scauzx.base.presenter.IBasePresenter;
 import com.scauzx.base.view.IBaseView;
 
